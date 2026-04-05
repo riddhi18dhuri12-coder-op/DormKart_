7b58754eb68e92a74c619bdfdd2d109d0ffd0690
riddhi18dhuri12-coder-op <riddhi18dhuri12@gmail.com>
2026-04-05 18:38:31 +0530

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-// js/config.js
-const SUPABASE_URL = 'https://bhfhtxmgncsyjlgsorap.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
-
-// Initialize the Supabase client
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
